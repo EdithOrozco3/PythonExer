7c97e36d641d865e9bfbd602d288c6bf1bc8f525
EdithOrozco3 <monedith033@outlook.com>
2025-06-22 21:32:41 -0600

    Add files via upload

---
+++ Listas.py
@@ -0,0 +1,40 @@
+frutas = ["manzana", "banana", "naranja"]   
+
+
+print(frutas[0])  # Imprime "manzana"
+print(frutas[1])  # Imprime "banana"
+print(frutas[2])  # Imprime "naranja"
+
+print(frutas[-1])  # Imprime "naranja"
+print(frutas[-2])  # Imprime "banana"
+print(frutas[-3])  # Imprime "manzana"
+
+frutas.append("pera")
+print(frutas)  # Imprime ["manzana", "banana", "naranja", "pera"]
+
+
+frutas.insert(1, "uva")
+print(frutas)  # Imprime ["manzana", "uva", "banana", "naranja", "pera"]
+
+
+frutas.remove("banana")
+print(frutas)  # Imprime ["manzana", "uva", "naranja", "pera"]
+
+
+fruta_eliminada = frutas.pop(2)
+print(frutas)  # Imprime ["manzana", "uva", "pera"]
+print(fruta_eliminada)  # Imprime "naranja"
+
+
+frutas.sort()
+print(frutas)  # Imprime ["manzana", "pera", "uva"]
+
+
+frutas.reverse()
+print(frutas)  # Imprime ["uva", "pera", "manzana"]
+
+#nueva_lista = [expresion for elemento in secuencia if condicion]
+
+numeros = [1, 2, 3, 4, 5]
+cuadrados = [n**2 for n in numeros]
+print(cuadrados)  # Imprime [1, 4, 9, 16, 25]
