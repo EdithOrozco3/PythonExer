fafa5359eca4a5ef4e72ef8b13b12c8c69528c07
EdithOrozco3 <monedith033@outlook.com>
2025-06-23 09:07:46 -0600

    Add files via upload

---
+++ Tuplas.py
@@ -0,0 +1,14 @@
+punto = (3, 4)
+
+print(punto[0])  # Imprime 3
+
+print(punto[1])  # Imprime 4
+
+mi_tupla = (1, 2, 3, 2, 4, 2)
+
+
+print (mi_tupla.index(2))   # Salida: 1
+
+print (mi_tupla.index(2, 2))   #Salida: 3
+
+print (mi_tupla.index(2, 2, 4))   #Salida: 3
